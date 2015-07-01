589577ddb397b0abcf6e2c3da55f7fcc7576c471
Sigurd Gartmann <sigurdga@sigurdga.no>
2015-07-01 21:13:12 +0200

    Remove unused grunt module loaders

--- Gruntfile.js
+++ Gruntfile.js
@@ -160,9 +160,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-i18n-abide');
     grunt.loadNpmTasks('grunt-webpack');
 
