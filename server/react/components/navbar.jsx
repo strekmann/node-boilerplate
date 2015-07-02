e3bd397e05d60a8ccbd9f2f160f2cf51e7659346
Sigurd Gartmann <sigurdga@sigurdga.no>
2015-07-02 21:53:18 +0200

    Navbar now uses data from UserStore

--- server/react/components/navbar.jsx
+++ server/react/components/navbar.jsx
@@ -1,23 +1,38 @@
 var React = require('react'),
+    FluxyMixin = require('alt/mixins/FluxyMixin'),
     Navbar = require('react-bootstrap/lib/Navbar'),
     Nav = require('react-bootstrap/lib/Nav'),
     NavItem = require('react-bootstrap/lib/NavItem'),
     UserStore = require('../stores/user.jsx');
 
-/**
-A simple component that doubles in size on click
-*/
 var TestNavbar = React.createClass({
+    mixins: [FluxyMixin],
+    displayName: 'TestNavbar',
 
-  render: function () {
-    return (
-        <Navbar brand="React-Test">
-            <Nav left>
-                <NavItem href="/auth/google">Log in</NavItem>
-            </Nav>
-        </Navbar>
-    );
-  }
+    getInitialState: function(){
+        var storeState = UserStore.getState();
+        return {
+            user: storeState.user
+        };
+    },
+
+    render: function () {
+        var userItem;
+        if (this.state.user) {
+            userItem = (<NavItem href="/account">{this.state.user.name}</NavItem>);
+        }
+        else {
+            userItem = (<NavItem href="/auth/google">Log in</NavItem>);
+        }
+
+        return (
+            <Navbar brand="React-Test">
+                <Nav left>
+                    {userItem}
+                </Nav>
+            </Navbar>
+        );
+    }
 });
 
 module.exports = TestNavbar;
