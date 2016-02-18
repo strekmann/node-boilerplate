2787249c33437900c72e5fae808ef51b94e6bf0b
Sigurd Gartmann <sigurdga@sigurdga.no>
2016-02-18 12:58:02 +0100

    Add toJSON method to display id instead of _id

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -21,6 +21,14 @@ UserSchema.set('toObject', {
     }
 });
 
+UserSchema.set('toJSON', {
+    transform: function (document, ret, options) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+    }
+});
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {
